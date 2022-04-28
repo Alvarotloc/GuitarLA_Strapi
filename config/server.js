16dfe28a9fc18040db1f3de89a65e3d1d7e07329
Alvarotloc <alvarotloc@gmail.com>
2022-04-28 13:03:24 +0200

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4bf4132fa596725cd2445388ff7a9d20'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7c63f267-1862-46f9-9779-3eea765a42ea'
+};
