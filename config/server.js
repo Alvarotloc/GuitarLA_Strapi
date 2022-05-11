2bf3e71b074537c418d89d4fc66315920ea22c2e
Alvarotloc <alvarotloc@gmail.com>
2022-05-11 12:48:54 +0200

    deploy

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth:{
-      secret: env('ADMIN_JWT_SECRET', '4bf4132fa596725cd2445388ff7a9d20'),
+      secret:env('ADMIN_JWT_SECRET', '4bf4132fa596725cd2445388ff7a9d20'),
     },
   },
 });
